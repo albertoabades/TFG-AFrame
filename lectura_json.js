04932bddde4d904a1d948ee8c3a15fff950ab9a4
albertoabades <alberto.abades@gmail.com>
2021-07-04 13:49:18 +0200

    Prueba para cargar un archivo JSON

---
+++ lectura_json.js
@@ -0,0 +1,20 @@
+// this requests the file and executes a callback with the parsed result once
+//   it is available
+fetchJSONFile('Prueba_Lectura.json', function(data){
+    // do something with your data
+    console.log(data);
+});
+
+function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send(); 
+}
